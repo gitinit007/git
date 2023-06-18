8baececaab75d96fdb4461029135bbb23a473263
K Vasu <vasukurnella@gmail.com>
2023-06-18 22:22:26 +0530

    simple calculator

---
+++ app.js
@@ -0,0 +1,30 @@
+document.getElementById("calculateBtn").addEventListener("click", function () {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  console.log(num1);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  console.log(num2);
+  let operation = document.getElementById("operation").value;
+  console.log(operation);
+
+  let result;
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "sub":
+      result = num1 - num2;
+      break;
+    case "mul":
+      result = num1 * num2;
+      break;
+    case "div":
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        result = "Cannot divide by zero";
+      }
+      break;
+  }
+
+  document.getElementById("result").innerText = result;
+});
